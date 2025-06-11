72980bc8e50fbe1899b7599b93157a1877b7378e
jackosman <osmanyetistirici@gmail.com>
2025-06-11 19:29:34 +0300

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,13 @@
+{
+  "name": "unblocker-proxy",
+  "version": "1.0.0",
+  "description": "A free web proxy using Node.js and Unblocker.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "unblocker": "^2.0.0"
+  }
+}
